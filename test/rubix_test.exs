5a6d1b595e138545227d9a58dba5828eab3b09d6
Flavio Auciello <flavioauciello@nebulab.com>
2023-03-25 14:51:14 +0100

    Create a Rubik cube and get faces

--- test/rubix_test.exs
+++ test/rubix_test.exs
@@ -1,8 +1,17 @@
 defmodule RubixTest do
   use ExUnit.Case
+  alias Rubix.Cube
+
   doctest Rubix
 
-  test "greets the world" do
-    assert Rubix.hello() == :world
+  test "generate a new Rubik cube (3x) and get faces (chars)" do
+    cube = Cube.new()
+
+    assert Cube.face(cube, :up) == ~w[R R R R R R R R R]
+    assert Cube.face(cube, :front) == ~w[B B B B B B B B B]
+    assert Cube.face(cube, :back) == ~w[Y Y Y Y Y Y Y Y Y]
+    assert Cube.face(cube, :left) == ~w[W W W W W W W W W]
+    assert Cube.face(cube, :right) == ~w[G G G G G G G G G]
+    assert Cube.face(cube, :down) == ~w[O O O O O O O O O]
   end
 end

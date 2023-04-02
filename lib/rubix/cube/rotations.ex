da001835dc91a57776b8a8b729c402bb9924c1f8
Flavio Auciello <flavioauciello@nebulab.com>
2023-04-02 17:29:53 +0200

    F

--- lib/rubix/cube/rotations.ex
+++ lib/rubix/cube/rotations.ex
@@ -49,6 +49,12 @@ defmodule Rubix.Cube.Rotations do
       {:left, 3, 1} => {:back, 3, 1}, {:left, 3, 2} => {:back, 3, 2}, {:left, 3, 3} => {:back, 3, 3},
       {:back, 3, 1} => {:right, 3, 1}, {:back, 3, 2} => {:right, 3, 2}, {:back, 3, 3} => {:right, 3, 3},
       {:right, 3, 1} => {:front, 3, 1}, {:right, 3, 2} => {:front, 3, 2}, {:right, 3, 3} => {:front, 3, 3}
+    },
+    "F" => %{
+      {:up, 3, 1} => {:right, 1, 1}, {:up, 3, 2} => {:right, 2, 1}, {:up, 3, 3} => {:right, 3, 1},
+      {:right, 1, 1} => {:down, 1, 3}, {:right, 2, 1} => {:down, 1, 2}, {:right, 3, 1} => {:down, 1, 1},
+      {:down, 1, 1} => {:left, 1, 3}, {:down, 1, 2} => {:left, 2, 3}, {:down, 1, 3} => {:left, 3, 3},
+      {:left, 1, 3} => {:up, 3, 3}, {:left, 2, 3} => {:up, 3, 2}, {:left, 3, 3} => {:up, 3, 1}
     }
   }
   @rotations Map.keys(@rotation_matrix)
